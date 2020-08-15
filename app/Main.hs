ef8e495024d9320746295fcec4f57df21d400169
P0ngCh4ng <44018880+P0ngCh4ng@users.noreply.github.com>
2020-08-15 19:56:57 +0900

    add Module ChampionMastery

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,19 @@
 module Main where
 
-import LoLDatas
 import API.Champion
-import API.Summoner
+import API.ChampionMastery
 import API.Spectator
+import API.Summoner
+import LoLDatas
 
 api_key = "RGAPI-c8c3e1ca-6254-4584-8150-c792d44847b3" --ADD YOUR API KEY
 
 main :: IO ()
 main = do
-    rotations <- champion_rotations api_key JP1
-    print rotations
-    summoner <- summoner_by_name api_key JP1 "ツナカユリコ" --ADD YOUR SOMMONER NAME
-    let (Just summoner_id) = LoLDatas.id <$> summoner
-    print summoner_id
-    current_game_info <- spectator_by_summoner api_key JP1 summoner_id
-    print current_game_info
+  rotations <- champion_rotations api_key JP1
+  print rotations
+  summoner <- summoner_by_name api_key JP1 "ツナカユリコ" --ADD YOUR SOMMONER NAME
+  let (Just summoner_id) = LoLDatas.id <$> summoner
+  print summoner_id
+  current_game_info <- spectator_by_summoner api_key JP1 summoner_id
+  print current_game_info
